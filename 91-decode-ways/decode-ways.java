8a8273e730db005b392e646791e7b3b897de0fbc
tanishkavrshney <183093446+tanishkavrshney@users.noreply.github.com>
2025-07-02 10:00:42 -0700

    Time: 1 ms (83.72%) | Memory: 42 MB (36.88%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        if (n == 0 || s.charAt(0) == '0') return 0;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+            int twoDigit = Integer.parseInt(s.substring(i - 2, i));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
