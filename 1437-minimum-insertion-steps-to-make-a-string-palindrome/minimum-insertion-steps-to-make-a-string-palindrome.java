b669ca56622b57440aeb37f1a3c320c1129408d4
tanishkavrshney <183093446+tanishkavrshney@users.noreply.github.com>
2025-09-28 11:14:28 -0700

    Time: 26 ms (53.76%) | Memory: 46.1 MB (5.24%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minInsertions(String s) {
+        return s.length() - longestPalindromeSubseq(s);
+    }
+    public int longestPalindromeSubseq(String s) {
+        String t=rev(s);
+        return longestCommonSubsequence(s,t);
+    }
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Build dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m]; // LCS length
+    }
+    public String rev(String s){
+        char[] arr = s.toCharArray();
+        
+        int left = 0, right = arr.length - 1;
+        while(left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        
+        String reversed = new String(arr);
+        return reversed;
+    }
+}
