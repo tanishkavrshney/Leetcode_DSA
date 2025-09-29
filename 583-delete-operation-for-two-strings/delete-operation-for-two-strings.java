86b68842408f51c23939a1eca6cce8e38ff1ef5e
tanishkavrshney <183093446+tanishkavrshney@users.noreply.github.com>
2025-09-29 00:09:09 -0700

    Time: 7 ms (69.20%) | Memory: 44.7 MB (74.19%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        return (word1.length()+word2.length()) - 2*longestCommonSubsequence(word1,word2);
+    }
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Build dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m]; // LCS length
+    }
+}
